ae808016bbf1747cfb17d52a80d4910bba6dc099
Andrew Choi <adc1225.dev@gmail.com>
2020-10-30 12:31:06 -0600

    updated seeds with uploaded images and audio

--- routes/api/albums.js
+++ routes/api/albums.js
@@ -5,7 +5,7 @@ const { Album, Track, User } = require("../../db/models");
 
 const router = express.Router();
 
-
+// GET all albums with users and tracks data
 router.get('/', asyncHandler(async (req, res) => {
 
   const allAlbums = await Album.findAll({
@@ -18,5 +18,23 @@ router.get('/', asyncHandler(async (req, res) => {
   res.json({allAlbums});
 
 }))
+
+
+// GET ONE album with user and tracks data
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+
+    const album = await Album.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {model: User, attributes: {exclude: ['email','hashedPassword']}},
+            {model: Track }
+        ]
+    })
+  
+    res.json({album});
+  
+  }))
 
 module.exports = router;
